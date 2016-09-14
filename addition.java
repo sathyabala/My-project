784a920ebc7d24cfe0e164284e782369be02a84a
sathyabala <sathya.mrkit@gmail.com>
2016-09-14 16:49:51 +0530

    addition program

---
+++ addition.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.scanner;
+class addition
+{
+	int a,b,c;
+	Scanner in =new Scanner(System.in);
+	c=in.nextInt();
+	System.out.println("Enter the number");
+	c=a+b;
+	System.out.println("Answer is");
+}
